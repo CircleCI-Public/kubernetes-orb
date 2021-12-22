08f9f1d719f3163ea4e2044f7bcd34eeefdf53fb
Brian Vu <emailbrian.vu@gmail.com>
2021-12-22 14:46:31 -0800

    fix: fixed variable

--- src/scripts/install-kops.sh
+++ src/scripts/install-kops.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 KOPS_VERSION=$(eval echo "$PARAM_KOPS_VERSION")
-if [[ KOPS_VERSION == "latest" ]]; then
+if [[ "$KOPS_VERSION" == "latest" ]]; then
     # get latest kops release
     KOPS_VERSION=$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)
 fi
@@ -12,9 +12,6 @@ fi
 
 # download kops
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$KOPS_VERSION/kops-$PLATFORM-amd64
-
-chmod +x kops
-sudo mv kops /usr/local/bin/kops
 
 [ -w /usr/local/bin ] && SUDO="" || SUDO=sudo
 
