1317a39cde31e3c5f516376b9219da68c4d6aea3
Brian Vu <emailbrian.vu@gmail.com>
2021-12-29 13:52:30 -0800

    feat: added --max-timeout flag

--- src/scripts/install-kubectl.sh
+++ src/scripts/install-kubectl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 KUBECTL_VERSION=$(eval echo "$PARAM_KUBECTL_VERSION")
+MAX_TIME=$(eval echo "$PARAM_MAX_TIME")
 if [[ "$KUBECTL_VERSION" == "latest" ]]; then
     # get latest kubectl release
     KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
@@ -11,7 +12,11 @@ if [ -n "$(uname | grep "Darwin")" ]; then
 fi
 
 # download kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/$PLATFORM/amd64/kubectl
+if [[ "$MAX_TIME" == "1" ]]; then
+    curl --max-time 300 -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/$PLATFORM/amd64/kubectl
+else 
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/$PLATFORM/amd64/kubectl
+fi
 
 [ -w /usr/local/bin ] && SUDO="" || SUDO=sudo
 
