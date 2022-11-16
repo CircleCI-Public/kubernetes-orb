583cf5b78056b438d803a40fb8913da97b2e7caa
Brian Vu <emailbrian.vu@gmail.com>
2022-11-16 14:08:21 -0800

    fix: addressed review errors

--- src/scripts/install-kubeconfig.sh
+++ src/scripts/install-kubeconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-KUBECONFIG=$(eval echo "$PARAM_KUBECONFIG")
+KUBECONFIG=$(eval echo "$ORB_ENV_KUBECONFIG")
 if [ -n "${KUBECONFIG}" ]; then
-    mkdir -p $HOME/.kube
-    echo -n "$KUBECONFIG" | base64 --decode > $HOME/.kube/config
+    mkdir -p "$HOME"/.kube
+    echo -n "$KUBECONFIG" | base64 --decode > "$HOME"/.kube/config
 fi
